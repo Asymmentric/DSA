ddfe65711cbb8003695663d55f9069fe87050407
Asymmentric <74967082+Asymmentric@users.noreply.github.com>
2021-11-18 21:17:10 +0530

    Duplicate Elements of an array
    Time Complexity :- O(n)

---
+++ Duplicate Elements .cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int a[]={7,7,7,1,2,3,3,3,4,4,5};
+	int n=sizeof(a)/sizeof(a[1]);
+
+	int i=0;
+	while(i<n){
+		if(a[i]==a[i+1]){
+			a[i]=NULL;
+		}
+		i++;
+	}
+	
+	for (int i = 0; i < n; i++) {
+		cout<<a[i]<<" ";
+	}
+	return 0;
+}
