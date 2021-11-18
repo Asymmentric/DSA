07bac3c64f4bac8244a0f97451642a5a9ea700b0
Asymmentric <74967082+Asymmentric@users.noreply.github.com>
2021-11-18 22:48:28 +0530

    (Bruteforce) Unique Element of an Array
    Time Complexity :- O(n^2)

---
+++ Unique Elements of Array/1 Unique Element.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int a[]={3,9,2,0,5,9,0,3,2};
+	int n=sizeof(a)/sizeof(a[1]);
+	
+	
+	for (int i = 0; i < n; i++) {
+		cout<<a[i]<<" ";
+	}
+
+	for(int i=0;i<n;i++){
+		int count=0;
+		for(int j=i+1;j<n;j++){
+			
+			if(a[i]==a[j]){
+				count=1;
+				break;
+			}
+			
+		}
+		if(count==0){
+			cout<<endl<<a[i];
+			break;
+		}
+		
+	}
+	return 0;
+}
