8dad1044a0c346fd74d77d92ec9780a2e33a32fd
Asymmentric <74967082+Asymmentric@users.noreply.github.com>
2021-11-18 22:16:58 +0530

    Insertion Sort
    Time Complexity :- O(n^2)

---
+++ Insertion Sort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int a[]={8,2,6,4,3,5};
+	int n=sizeof(a)/sizeof(a[1]);
+
+	//insertion sort 
+	
+	for(int i=1;i<n;i++){
+		int j=i;
+		int temp=a[i];
+		while(a[j-1]>temp && j>=0){
+			a[j]=a[j-1];
+			j--;
+		}
+		a[j]=temp;	
+	}
+	
+	for (int i = 0; i < n; i++) {
+		cout<<a[i]<<" ";
+	}
+	return 0;
+}
